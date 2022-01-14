aa9961a41c3d68b63f7f7bc486d16756acb43075
jonask028 <jonas.kennedy.028@gmail.com>
2022-01-13 22:41:11 -0700

    Finished

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,9 +1,25 @@
-out1 = "It is certain"
-out2 = "Outlook good"
-out3 = "You may rely on it"
-out4 = "Ask again later"
-out5 = "Concentrate and ask again"
-out6 = "Reply hazy, try again"
-out7 = "My reply is no"
-out8 = "My sources say no"
+from curses import KEY_ENTER
+from turtle import onkeypress
+import random
 
+out0 = "It is certain"
+out1 = "Outlook good"
+out2 = "You may rely on it"
+out3 = "Ask again later"
+out4 = "Concentrate and ask again"
+out5 = "Reply hazy, try again"
+out6 = "My reply is no"
+out7 = "My sources say no"
+prompt = [out0,out1,out2,out3,out4,out5,out6,out7]
+i = 1
+def magic_8_ball(i):
+    while True:
+        user_input = input("What would you like to know? (Push enter to exit) \n")
+        if user_input:
+            answer = random.choice(prompt)
+            print(f' \n{answer}\n')
+        else:
+            print("bye!") 
+            i = i - 1
+            break
+magic_8_ball(1)
