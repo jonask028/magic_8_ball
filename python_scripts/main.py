df56bcc94a31600f8c0ca4b6071ff75e1b979409
jonask028 <jonas.kennedy.028@gmail.com>
2022-01-13 23:14:46 -0700

    Changed name of variable

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -10,13 +10,13 @@ out4 = "Concentrate and ask again"
 out5 = "Reply hazy, try again"
 out6 = "My reply is no"
 out7 = "My sources say no"
-prompt = [out0,out1,out2,out3,out4,out5,out6,out7]
+responses = [out0,out1,out2,out3,out4,out5,out6,out7]
 i = 1
 def magic_8_ball(i):
     while True:
         user_input = input("What would you like to know? (Push enter to exit) \n")
         if user_input:
-            answer = random.choice(prompt)
+            answer = random.choice(responses)
             print(f' \n{answer}\n')
         else:
             print("bye!") 
